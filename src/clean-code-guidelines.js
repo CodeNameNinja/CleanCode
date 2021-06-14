62f7a6591d1df49a12870a451a2afcac712b6f38
Mitchell Yuen <mitchell.yuen@lavalamp.biz>
2021-06-14 19:03:01 +0200

    docs: add documentation for primatives, functions, classes and modules

---
+++ src/clean-code-guidelines.js
@@ -0,0 +1,12 @@
+/*
+@class
+*/
+class User {
+    constructor(userData){
+        this.name = userData.name;
+        this.age = userData.age;
+        this.email = userData.email;
+    }
+}
+
+const user = new User();
---
+++ src/jsdoc-cheatsheet.js
@@ -0,0 +1,93 @@
+const { add, subtract } = require('./jsdoc-cheatsheet-module');
+/**
+ * Student Name
+ * @type {string}
+ */
+const studentName = "Mitchell Yuen";
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+/**
+ * @type {{id:number|string, text: string}}
+ */
+const todo = {
+    id: 1,
+    text: "hello"
+}
+
+// Function and Params
+
+/**
+ * Calculate tax 
+ * @param {number} amount - Total amount
+ * @param {number} tax - Tax percentage
+ * @returns {string}  Total with a dollar sign
+ */
+const calculateTax = (amount, tax) => { 
+    return `$${amount + tax*amount}`
+}
+
+console.log(calculateTax(100,0.1))
+
+/** 
+ * A student
+ * @typedef {Object} Student
+ * @property {number} id - Student Id
+ * @property {string} name - Student name
+ * @property {string|number} [age] - Student age
+ * @property {boolean} isActive - Student is active
+*/
+
+/** 
+ * @type {Student}
+*/
+const student = {
+    id: 1,
+    name: "John",
+    age: 20,
+    isActive: true
+}
+
+/**
+ * Class to create a person object
+ */
+class Person {
+    /**
+     * 
+     * @param {Object} personInfo Information about the person
+     */
+    constructor(personInfo){
+        /**
+         * @property {string} name Persons name
+         */
+        this.name = personInfo.name;
+        /**
+         * @property {string} age Persons age
+         */
+        this.age = personInfo.age;
+    }
+    /**
+     * @property {Function} greet - A greeting with name and age
+     * @returns {string} - A greeting with name and age
+     */
+    greet(){
+        return `Hello, my name is ${this.name} and I am ${this.age}`
+    }
+}
+
+/**
+ * Person one
+ * see {@link Person}
+ */
+const person1 = new Person({
+    name: 'John',
+    age:20
+});
+
+console.log(person1.greet());
+
+console.log(add(20, 50))
